d0b255522419409f2596cf00540a1ab61d06bde3
giercko <giercko.contreras.12@sansano.usm.cl>
2016-12-05 05:04:17 -0300

    problemas en el doinbackground

--- app/src/main/java/com/example/stephie/proyectomultimedios/Connections/MyAsyncTaskExecutor.java
+++ app/src/main/java/com/example/stephie/proyectomultimedios/Connections/MyAsyncTaskExecutor.java
@@ -1,8 +1,13 @@
 package com.example.stephie.proyectomultimedios.Connections;
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.stephie.proyectomultimedios.Datos;
 import com.example.stephie.proyectomultimedios.MainActivity;
+import com.example.stephie.proyectomultimedios.Presenter.MainPresenter;
 
 /**
  * Created by neo_free on 05/12/2016.
@@ -13,7 +18,7 @@ public class MyAsyncTaskExecutor {
     //private RecyclerView.Adapter mAdapter;
 
     private static MyAsyncTaskExecutor instance;
-
+    private Context ctx;
     public static MyAsyncTaskExecutor getInstance() {
         if (instance == null) {
             instance = new MyAsyncTaskExecutor();
@@ -22,7 +27,8 @@ public class MyAsyncTaskExecutor {
     }
 
     //public void executeMyAsynctask(final MainActivity activity, final RecyclerView mRecyclerView, final MainPresenterImpl presenter, final Database dbInstance) {
-    public void executeMyAsynctask (){
+    public void executeMyAsynctask(final Context ctx, final MainPresenter presenter, final TextView al_disponibles, final TextView al_utilizados, final TextView al_totales, final TextView ce_disponibles, final TextView ce_utilizados, final TextView ce_totales){
+        this.ctx = ctx;
         AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
 
             @Override
@@ -31,8 +37,12 @@ public class MyAsyncTaskExecutor {
             }
 
             @Override
-            protected String doInBackground(Void... params) {
-                String resultado = new HttpServerConnection().connectToServer("http://www.mocky.io/v2/582eea8b2600007b0c65f068", 15000);
+            protected String doInBackground(Void... params) { // AQUI SE CAE NO SE PORQUE ;·;
+                Toast.makeText(ctx,
+                        "pre coneccion ", Toast.LENGTH_LONG).show();
+                String resultado = new HttpServerConnection().connectToServer("http://www.mocky.io/v2/57f56f722500006e1f134987", 15000);
+                Toast.makeText(ctx,
+                        "pos coneccion ", Toast.LENGTH_LONG).show();
                 return resultado;
             }
 
@@ -40,14 +50,18 @@ public class MyAsyncTaskExecutor {
             protected void onPostExecute(String result) {
                 if (result != null) {
                     System.out.println(result);
-
-                    //Why god... why
-                    //mAdapter = new DataAdapter(activity, presenter.getFeeds(result), dbInstance);
-                    //mRecyclerView.setAdapter(mAdapter);
+                    Toast.makeText(ctx,
+                            "post asy ", Toast.LENGTH_LONG).show();
+                    Datos datos = presenter.getDatos(result);
+                    al_disponibles.setText("Disponibles: "+datos.getDisponiblesAL());
+                    al_totales.setText("Totales: "+datos.getTotalAL());
+                    al_utilizados.setText("Utilizados: "+datos.getUtilizadosAL());
+                    ce_disponibles.setText("Disponibles: "+datos.getDisponiblesCE());
+                    ce_totales.setText("Totales: "+datos.getTotalCE());
+                    ce_utilizados.setText("Utilizados: "+datos.getUtilizadosCE());
                 }
             }
         };
-
         task.execute();
     }
 }
--- app/src/main/java/com/example/stephie/proyectomultimedios/MainActivity.java
+++ app/src/main/java/com/example/stephie/proyectomultimedios/MainActivity.java
@@ -2,16 +2,41 @@ package com.example.stephie.proyectomultimedios;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.stephie.proyectomultimedios.Presenter.MainPresenter;
+import com.example.stephie.proyectomultimedios.Presenter.MainPresenterImpl;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView al_totales;
     private TextView al_utilizados;
-    private TextView
+    private TextView al_disponibles;
+    private TextView ce_totales;
+    private TextView ce_utilizados;
+    private TextView ce_disponibles;
+    private Button detalles_al;
+    private Button detalles_ce;
+
+    private MainPresenter presenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        al_disponibles = (TextView) findViewById(R.id.al_disponibles);
+        al_totales = (TextView)findViewById(R.id.al_total);
+        al_utilizados = (TextView)findViewById(R.id.al_utilizados);
+        ce_totales = (TextView)findViewById(R.id.ce_total);
+        ce_utilizados = (TextView)findViewById(R.id.ce_utilizados);
+        ce_disponibles = (TextView)findViewById(R.id.ce_disponibles);
+        detalles_al = (Button)findViewById(R.id.detalles_al);
+        detalles_ce = (Button)findViewById(R.id.detalles_ce);
+
+        al_disponibles.setText("Disponibles: its over 9000!!");
+        presenter = new MainPresenterImpl(this, al_disponibles,al_utilizados, al_totales, ce_disponibles, ce_utilizados, ce_totales);
+
     }
 }
